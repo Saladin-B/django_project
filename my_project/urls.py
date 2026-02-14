a7a5b87153610ad327413eaf19619559e8a5cfbc
Saladin-B <saladeen.baalache@gmail.com>
2026-02-14 15:35:19 +0000

    hello_world redone settings, url, and views.

--- my_project/urls.py
+++ my_project/urls.py
@@ -2,7 +2,7 @@
 URL configuration for my_project project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -17,10 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from hello_world import views as index_views
-from about import views as about_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),
-    path('about/', about_views.about_me, name='about'),
     path('admin/', admin.site.urls),
+    path('', index_views.index, name='index'),
 ]
